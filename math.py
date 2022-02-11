f2eca766e8b77f5caff085d6f67b95dff869e691
TejaAmbrale <99359146+TejaAmbrale@users.noreply.github.com>
2022-02-11 23:08:33 +0530

    initial commit mat.py

---
+++ math.py
@@ -0,0 +1,28 @@
+#add implementation
+def add(x,y):
+    return x+y
+
+#substract implementation
+def substract(x,y):
+    return x-y
+
+#multiply implementation
+def multiply(x,y):
+    return x*y          #on Bug456
+
+#divide implementation
+def diviidss(x,y):
+
+    if(y==0):
+        return DIVIDE_BY_0_RROR
+    else:
+        return x/y          #on bug
+
+def div(x,y):
+    if(y==0):
+        return eroe
+    else:
+        return x/y 
+
+def square(x):
+    pass
